b22cea909399b6206b9bbedac393d3eb8f75cc5e
David Huang <huang.z.david@gmail.com>
2017-12-09 14:44:22 -0800

    Updated Menus + Formatting

--- TravelersGuide.java
+++ TravelersGuide.java
@@ -18,14 +18,17 @@ import java.util.concurrent.TimeUnit;
  *
  * @author david
  */
-public class TravelersGuide {
+public class TravelersGuide{
 
 	static final String DB_URL = "jdbc:mysql://localhost:3306/TravelersGuide";
 	static final String DB_DRV = "com.mysql.jdbc.Driver";
 	static final String DB_USER = "root";
 	static final String DB_PASSWD = "password";// "1frjxqsF4/t";
+	static String user_userID = "";
+	static String user_Language = "";
+	static String user_Country ="";
 
-	private static void loginMenu(Scanner reader, Connection conc,
+	private static void loginMenu(Connection conc, Scanner reader,
 			Statement stmt, ResultSet rs) throws InterruptedException,
 			SQLException {
 		System.out.print("First Name: ");
@@ -34,19 +37,22 @@ public class TravelersGuide {
 		String lName = reader.nextLine();
 		System.out.print("Password: ");
 		String password = reader.nextLine();
-		String SQL = "SELECT userID, psw FROM Users WHERE firstname=\"" + fName
+		String SQL = "SELECT userID, psw, language, country FROM Users WHERE firstname=\"" + fName
 				+ "\" and lastname=\"" + lName + "\";";
 		stmt = conc.createStatement();
 		rs = stmt.executeQuery(SQL);
 
 		while (rs.next()) {
 			if (password.equals(rs.getString(2))) {
+				user_userID = rs.getString(1);
+				user_Language = rs.getString(3);
+				user_Country = rs.getString(4);
 				if (rs.getInt(1) == -1) {
 					// TODO: Send admin to admin menus as well
 					System.out.println("Welcome administrator.");
 					System.out
 							.println("List of admin options here, leading to functions.");
-					travelersMainMenu(reader, conc, stmt, rs);
+					travelersMainMenu(conc, reader);
 				} else {
 					System.out.println("Login success!");
 					System.out
@@ -58,7 +64,7 @@ public class TravelersGuide {
 		System.out.println("Failure to login.");
 	}
 
-	private static void guestMenu(Scanner reader, Connection conc,
+	private static void guestMenu(Connection conc, Scanner reader,
 			Statement stmt, ResultSet rs) throws InterruptedException,
 			SQLException {
 		// TODO Remove login on guestmenu, or remove guest entirely
@@ -66,11 +72,11 @@ public class TravelersGuide {
 				+ "Admin" + "\" and lastname=\"" + "Istrator" + "\";";
 		stmt = conc.createStatement();
 		rs = stmt.executeQuery(SQL);
-		travelersMainMenu(reader, conc, stmt, rs);
+		travelersMainMenu(conc, reader);
 	}
 
-	public static void travelersMainMenu(Scanner reader, Connection conc,
-			Statement stmt, ResultSet rs) throws InterruptedException,
+	public static void travelersMainMenu(Connection conc, Scanner reader
+			) throws InterruptedException,
 			SQLException {
 
 		System.out.println("*********************************");
@@ -98,19 +104,19 @@ public class TravelersGuide {
 
 		String option = reader.nextLine();
 		if (option.equals("1")) {
-			travelersUserMenu(reader, conc, stmt, rs);
+			travelersUserMenu(conc, reader,user_userID,user_Language,user_Country);
 		}
 		if (option.equals("2")) {
-			travelersCountryMenu(reader, conc, stmt, rs);
+			travelersCountryMenu(conc, reader, user_userID,user_Language,user_Country);
 		}
 		if (option.equals("3")) {
-			travelersAdminMenu(reader, conc, stmt, rs);
+			travelersAdminMenu(conc, reader);
 		}
 
 	}
 
-	public static void travelersUserMenu(Scanner reader, Connection conc,
-			Statement stmt, ResultSet rs) throws InterruptedException,
+	public static void travelersUserMenu(Connection conc, Scanner reader,
+			 String userID, String language, String country) throws InterruptedException,
 			SQLException {
 
 		System.out.println("");
@@ -122,11 +128,10 @@ public class TravelersGuide {
 		System.out.println("* 3. View Users w/ Same Language*");
 		System.out.println("* 4. Add Connection             *");
 		System.out.println("* 5. Delete Connection          *");
-		System.out.println("* 6. View Connections           *");
+		System.out.println("* 6. View Own Connections       *");
 		System.out.println("* 7. Add Recommendation         *");
 		System.out.println("* 8. View Recommendations       *");
-		System.out.println("* 9. View Distance to Country   *");
-		System.out.println("* 10. View Distance to User     *");
+		System.out.println("* 9. Reset Password             *");
 		System.out.println("*                               *");
 		System.out.println("* 0. Return to Main Menu        *");
 		System.out.println("*********************************");
@@ -140,38 +145,36 @@ public class TravelersGuide {
 			// TODO: Uncomment functions as they are added.
 
 			if (option.equals("1")) {
-				// funcUser1(reader, conc, stmt, rs);
+				// funcUser1(conc, reader, );
 			} else if (option.equals("2")) {
-				funcUser2(reader, conc, stmt, rs);
+				//funcUser2(conc, reader, );
 			} else if (option.equals("3")) {
-				// funcUser3(reader, conc, stmt, rs);
+				// funcUser3(conc, reader, );
 			} else if (option.equals("4")) {
-				// funcUser4(reader, conc, stmt, rs);
+				// funcUser4(conc, reader, );
 			} else if (option.equals("5")) {
-				// funcUser5(reader, conc, stmt, rs);
+				// funcUser5(conc, reader, );
 			} else if (option.equals("6")) {
-				// funcUser6(reader, conc, stmt, rs);
+				// funcUser6(conc, reader, );
 			} else if (option.equals("7")) {
-				funcUser7(reader, conc, stmt, rs);
+				//funcUser7(conc, reader, );
 			} else if (option.equals("8")) {
-				funcUser8(reader, conc, stmt, rs);
+				//funcUser8(conc, reader, );
 			} else if (option.equals("9")) {
-				// funcUser9(reader, conc, stmt, rs);
-			} else if (option.equals("10")) {
-				// funcUser10(reader, conc, stmt, rs);
+				// funcUser9(conc, reader, );
 			} else if (option.equals("0")) {
-				travelersMainMenu(reader, conc, stmt, rs);
+				travelersMainMenu(conc, reader);
 				break;
 			} else {
-				travelersUserMenu(reader, conc, stmt, rs);
+				travelersUserMenu(conc, reader,user_userID,user_Language,user_Country);
 				break;
 			}
 		}
 
 	}
 
-	public static void travelersCountryMenu(Scanner reader, Connection conc,
-			Statement stmt, ResultSet rs) throws InterruptedException,
+	public static void travelersCountryMenu(Connection conc, Scanner reader,
+			String userID, String language, String country) throws InterruptedException,
 			SQLException {
 		System.out.println("");
 		System.out.println("*********************************");
@@ -182,8 +185,8 @@ public class TravelersGuide {
 		System.out.println("* 3. Compare Data b/w Countries *");
 		System.out.println("* 4. View Distances to Others   *");
 		System.out.println("* 5. View Bordering Countries   *");
-		System.out.println("* 6. View Recommended Countries *");
-		System.out.println("* 7. View Distance from Country *");
+		System.out.println("* 6. View Distance from Country *");
+		System.out.println("* 7. View Distance from C to C  *");
 		System.out.println("* 8. View Distance from User    *");
 		System.out.println("*                               *");
 		System.out.println("* 0. Return to Main Menu        *");
@@ -198,45 +201,42 @@ public class TravelersGuide {
 			// TODO: Uncomment functions as they are added.
 
 			if (option.equals("1")) {
-				// funcCountry1(reader, conc, stmt, rs);
+				// funcCountry1(conc, reader;
 			} else if (option.equals("2")) {
-				// funcCountry2(reader, conc, stmt, rs);
+				// funcCountry2(conc, reader;
 			} else if (option.equals("3")) {
-				// funcCountry3(reader, conc, stmt, rs);
+				// funcCountry3(conc, reader;
 			} else if (option.equals("4")) {
-				// funcCountry4(reader, conc, stmt, rs);
+				// funcCountry4(conc, reader;
 			} else if (option.equals("5")) {
-				funcCountry5(reader, conc, stmt, rs);
+                                // funcCountry5(reader,conc
 			} else if (option.equals("6")) {
-				// funcCountry6(reader, conc, stmt, rs);
+				// funcCountry6(conc, reader;
 			} else if (option.equals("7")) {
-				// funcCountry7(reader, conc, stmt, rs);
+				// funcCountry7(conc, reader;
 			} else if (option.equals("8")) {
-				// ffuncCountry8(reader, conc, stmt, rs);
-			} else if (option.equals("9")) {
-				// funcCountry9(reader, conc, stmt, rs);
-			} else if (option.equals("10")) {
-				// funcCountry10(reader, conc, stmt, rs);
+				// ffuncCountry8(conc, reader,;
 			} else if (option.equals("0")) {
-				travelersMainMenu(reader, conc, stmt, rs);
+				travelersMainMenu(conc, reader);
 				break;
 			} else {
-				travelersCountryMenu(reader, conc, stmt, rs);
+				travelersCountryMenu(conc, reader,user_userID,user_Language,user_Country);
 				break;
 			}
 		}
 
 	}
 
-	public static void travelersAdminMenu(Scanner reader, Connection conc,
-			Statement stmt, ResultSet rs) throws InterruptedException,
+	public static void travelersAdminMenu(Connection conc, Scanner reader
+			) throws InterruptedException,
 			SQLException {
 		System.out.println("");
 		System.out.println("*********************************");
 		System.out.println("*         Admin Menu            *");
 		System.out.println("*                               *");
 		System.out.println("* 1. View All Users             *");
-		System.out.println("* 2. Add/Delete Users           *");
+		System.out.println("* 2. Add Users                  *");
+		System.out.println("* 3. Delete User                *");
 		System.out.println("* 3. View all Connections       *");
 		System.out.println("* 4. View all Recommendations   *");
 		System.out.println("*                               *");
@@ -248,72 +248,7 @@ public class TravelersGuide {
 		Integer menuOption = reader.nextInt();
 
 		if (menuOption == 0) {
-			travelersMainMenu(reader, conc, stmt, rs);
-		}
-	}
-
-	private static void funcUser2(Scanner reader, Connection conc,
-			Statement stmt, ResultSet rs) throws SQLException {
-		System.out.print("Find what users are from which country: ");
-		String countryInput = reader.nextLine();
-		System.out.println(countryInput);
-		String SQL = "SELECT firstname,lastname,age FROM Users WHERE Users.country = ?";
-		PreparedStatement pstmt = conc.prepareStatement(SQL);
-		pstmt.setString(1, countryInput);
-		rs = pstmt.executeQuery();
-		while (rs.next()) {
-			System.out.printf("%s\t%s\t%d\t\n", rs.getString(1),
-					rs.getString(2), rs.getInt(3));
-		}
-		pstmt.close();
-	}
-
-	private static void funcUser7(Scanner reader, Connection conc,
-			Statement stmt, ResultSet rs) throws SQLException {
-		System.out.print("Create a new recommendation, enter in uID: ");
-		Integer uIDInput = reader.nextInt();
-		String whiteSpaec = reader.nextLine();
-		System.out.print("Enter in country: ");
-		String countryInput = reader.nextLine();
-		System.out.print("Enter in stars: ");
-		Integer starsInput = reader.nextInt();
-		whiteSpaec = reader.nextLine();
-		System.out.print("Enter in the recommendation: ");
-		String recInput = reader.nextLine();
-		String SQL = "INSERT into Recommendation(userID, name,stars,opinion)"
-				+ "VALUES (?,?,?,?)";
-		PreparedStatement pstmt = conc.prepareStatement(SQL);
-		pstmt.setInt(1, uIDInput);
-		pstmt.setString(2, countryInput);
-		pstmt.setInt(3, starsInput);
-		pstmt.setString(4, recInput);
-		pstmt.executeUpdate();
-	}
-
-	private static void funcUser8(Scanner reader, Connection conc,
-			Statement stmt, ResultSet rs) throws SQLException {
-		System.out.println("Current Recommendations: ");
-		// statement = connection.createStatement();
-		String SQL = "SELECT userID,name,stars,opinion FROM Recommendation";
-		PreparedStatement pstmt = conc.prepareStatement(SQL);
-		rs = pstmt.executeQuery();
-		while (rs.next()) {
-			System.out.printf("%d\t%s\t%d\t%s\t\n", rs.getInt(1),
-					rs.getString(2), rs.getInt(3), rs.getString(4));
-		}
-	}
-
-	private static void funcCountry5(Scanner reader, Connection conc,
-			Statement stmt, ResultSet rs) throws SQLException {
-		System.out
-				.print("Please enter in the country for the boundaries you wish to know: ");
-		String countryInput = reader.nextLine();
-		String SQL = "SELECT name2 FROM Boundaries WHERE Boundaries.name1 = ?";
-		PreparedStatement pstmt = conc.prepareStatement(SQL);
-		pstmt.setString(1, countryInput);
-		rs = pstmt.executeQuery();
-		while (rs.next()) {
-			System.out.printf("%s\t\n", rs.getString(1));
+			travelersMainMenu(conc, reader);
 		}
 	}
 
@@ -338,9 +273,9 @@ public class TravelersGuide {
 				System.out.printf("1)Login\n2)Guest\n3)Exit\n>");
 				String option = reader.nextLine();
 				if (option.equals("1") || option.equals("Login")) {
-					loginMenu(reader, connection, statement, resultSet);
+					loginMenu(connection,reader,statement,resultSet);
 				} else if (option.equals("2") || option.equals("Guest")) {
-					guestMenu(reader, connection, statement, resultSet);
+					guestMenu(connection,reader,statement,resultSet);
 				} else if (option.equals("3") || option.equals("Exit")) {
 					if (statement != null)
 						statement.close();
@@ -365,4 +300,3 @@ public class TravelersGuide {
 	}
 }
 // TODO code application logic here
-
